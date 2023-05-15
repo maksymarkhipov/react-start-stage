6f1df0cbdb8165b5e88b839ecb99ceb4776a8e2c
Rostyslav Liashenko <54037162+Kivi27@users.noreply.github.com>
2023-05-15 16:49:49 +0300

    feat: add single product page component

--- src/components/Routes/AppRoutes.tsx
+++ src/components/Routes/AppRoutes.tsx
@@ -1,12 +1,14 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { Home } from '../../page/home/Home';
 import { CategoryPage } from '../../page/category-page/CategoryPage';
+import { SingleProductPage } from '../../page/SingleProductPage/SingleProductPage';
 
 export const AppRoutes = () => {
   return (
       <Routes>
           <Route path='/' element={<Home />} />
           <Route path='category/:categoryTitle/' element={<CategoryPage />} />
+          <Route path='/product/:productId' element={<SingleProductPage />} />
           <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
   );
---
+++ src/page/SingleProductPage/SingleProductPage.tsx
@@ -0,0 +1,13 @@
+
+import styles from '../home/Home.module.css'
+
+export const SingleProductPage = () => {
+
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.content}>
+                <></>
+            </div>
+        </div>
+    );
+}
